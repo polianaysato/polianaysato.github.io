3665bb261af70e3c0eca68027c4a5266ef590a6b
Poliana <polianaysato@gmail.com>
2020-08-06 18:22:55 -0300

    form

--- js/contact.js
+++ js/contact.js
@@ -40,6 +40,21 @@ $(function () {
                         $('#contact-form')[0].reset();
                     }
                 }
+                complete: function (data) {
+
+                    var messageText = "Thank you for your contact.";
+
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+                    // If we have messageAlert and messageText
+                    if (messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
             });
             return false;
         }
